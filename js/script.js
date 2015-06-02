49a8efc88b50e82c63e9513e441c6192c8736843
Elliott Hughes-Taylor <ehughestaylor@Elliotts-MacBook-Pro.local>
2015-06-02 10:53:44 -0400

    alot of stuff

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+function navAnim(){
+    $('.button-collapse').on('click',function(){
+      $('#slide-out li a').addClass('animated slideInLeft');
+    });
+}
+
+
   // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
@@ -6,10 +13,18 @@
 
     $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    
+    //on window resize remove
+    $( window ).resize(function() {
+      $( "#log" ).append( "<div>Handler for .resize() called.</div>" );
+    });
+
     $('.modal-trigger').leanModal();
 
 
     $('.img-card').on('click', function(){
       $(this).removeClass('card');
     });
+
+    navAnim();
   });
